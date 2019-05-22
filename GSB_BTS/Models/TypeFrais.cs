286d881b844d5f0650aba20925fac1dd191f98c3
Aurélien Bellec <aurelien.bellec@live.fr>
2019-05-22 12:14:51 +0200

    Ajout TypeFrais

---
+++ GSB_BTS/Models/TypeFrais.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GSB.Models
+{
+    public class TypeFrais
+    {
+        public enum Frais { Hébergement, Restauration, Transport }
+
+        public Frais typeFraisCommercial { get => typeFraisCommercial; set => typeFraisCommercial = value; }
+
+        public TypeFrais()
+        {
+
+        }
+
+        public TypeFrais(Frais type)
+        {
+            this.typeFraisCommercial = type;
+        }
+    }
+}
